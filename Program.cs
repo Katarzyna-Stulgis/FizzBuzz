a60ed9dfc3385b87d5fb1898e3856df475a17be5
DESKTOP-6F4ETA7\Kasia <kasia2011r@gmail.com>
2023-11-06 15:47:28 +0100

    feat(main): add basic FizzBuzz implementation
    This change adds a basic implementation of the FizzBuzz algorithm, which checks numbers from 1 to 100 and displays 'Fizz', 'Buzz' or 'FizzBuzz' depending on the conditions.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace FizzBuzz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1; i < 100; i++)
+            {
+                Console.WriteLine(GenerateFizzBuzz(i));
+            }
+        }
+
+        public static string GenerateFizzBuzz(int number)
+        {
+            var result = String.Empty;
+
+            if (number % 3 == 0)
+            {
+                result += "Fizz";
+            }
+            if (number % 5 == 0)
+            {
+                result += "Buzz";
+            }
+            if(result.Length == 0)
+            {
+                result = number.ToString();
+            }
+
+            return result;
+        }
+    }
+}
