9a628bd1ac1c93f3218e7737295a2c7ed7a0dbea
DESKTOP-6F4ETA7\Kasia <kasia2011r@gmail.com>
2023-11-07 11:47:57 +0100

    add fizzbuzz.cs

---
+++ FizzBuzz.cs
@@ -0,0 +1,26 @@
+﻿namespace FizzBuzz
+{
+    public class FizzBuzz
+    {
+        public Dictionary<int, string> conditions = new Dictionary<int, string>
+        {
+            {3,"Fizz" },
+            {5,"Buzz" },
+            {7,"Whizz" },
+            {11,"Bang" },
+            {10, "Tizz" }
+        };
+
+
+        public string GenerateFizzBuzz(int number)
+        {
+            var fizzBuzzNumber = conditions
+                .Where(condition => number % condition.Key == 0)
+                .Select(condition => condition.Value)
+                .DefaultIfEmpty(number.ToString())
+                .ToArray();
+
+            return String.Join("", fizzBuzzNumber);
+        }
+    }
+}
